40759aea170198392016c02d82a87af1b924b642
Kamal Agayev <95571204+AgayevKamal@users.noreply.github.com>
2023-11-10 18:37:18 +0400

    Add files via upload

---
+++ comment.py
@@ -0,0 +1,19 @@
+# comments.py
+
+import sqlite3
+import streamlit as st
+
+def show_comments():
+    # Veritabanı bağlantısı
+    conn = sqlite3.connect('test3.db')
+    cursor = conn.cursor()
+
+    # Yorumları göster
+    st.header("Yorumlar")
+    comments = cursor.execute("SELECT * FROM comments").fetchall()
+
+    for comment in comments:
+        st.write(f"**{comment[1]}** diyor: {comment[2]}")
+
+    # Veritabanı bağlantısını kapat
+    conn.close()
